175f1b9d9387178484d206819f3ce237f19dda56
barvenov <barvenovsa@gmail.com>
2015-04-14 12:53:41 +0300

    замечания к коду

--- LabRab3/complex.h
+++ LabRab3/complex.h
@@ -8,11 +8,11 @@ struct complex
 	public:
 		complex(double real, double complex) : realPart(real), complexPart(complex)
 			{}
-		complex() : realPart(0), complexPart(0)
+		complex() : realPart(0.0), complexPart(0.0)
 			{}
-		complex(double real) : realPart(real), complexPart(0)
+		complex(double real) : realPart(real), complexPart(0.0)
 			{}
-		friend std::ostream& operator<< (std::ostream&, complex);
+		friend std::ostream& operator<< (std::ostream&, complex); //правильнее передавать  как const complex& и так везде, где можно
 		friend complex operator>> (std::istream&, complex&);
 		void operator=(double);
 		void operator+=(complex);
@@ -23,8 +23,9 @@ struct complex
 		void operator*=(double);
 		void operator/=(complex);
 		void operator/=(double);
-		complex operator*(complex);
-		complex operator*(double);
+		complex operator*(complex) /* const */; // очень приветствуется именно так
+		// нет оператора умножения и деления double на complex 
+		complex operator*(double); 
 		complex operator/(complex);
 		complex operator/(double);
 		friend complex operator+(double, complex);
@@ -33,7 +34,7 @@ struct complex
 		friend complex operator-(complex, complex);
 		friend complex operator+(complex, double);
 		friend complex operator-(complex, double);
-		bool operator==(complex);
-		friend double abs(complex);
+		bool operator==(complex); // логично было писать и оператор НЕравно тоже
+		friend double abs(complex); // тогда и аргумент делай...
 		friend complex conj(complex);
 };
