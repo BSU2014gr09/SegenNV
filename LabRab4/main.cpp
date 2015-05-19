ea20967ab36011de010abb6df1caa6463b6e1def
nsegen <segennikita@gmail.com>
2015-05-19 13:22:44 +0300

    labRab4

--- LabRab4/main.cpp
+++ LabRab4/main.cpp
@@ -6,51 +6,57 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	stack PRN; //нужно RPN, но я перепутал
-	//int n = 100;
-	char * c = "4,6 8,9 + 2 * 7,9 4 6,5 - / -";
-	char * ptrEnd;
-	float tmp;
-	while (*c)
-	{
-		if (*c == 0)  break; 
-		switch (*c)
+	stack PRN;
+	int n = 100;
+	while (1){
+		char * str = new char[100];
+		cin.getline(str, 100);
+		//cout << str;
+		char * ptrEnd;
+		char * c = str;
+		float tmp;
+		while (*c)
 		{
-		case '+':
-			tmp = PRN.pop();
-			tmp += PRN.pop();
-			PRN.push(tmp);
-			c++;
-			break;
-		case '-':
-			tmp = PRN.pop();
-			PRN.push(PRN.pop()-tmp);
-			c++;
-			break;
-		case '*':
-			tmp = PRN.pop();
-			tmp *= PRN.pop();
-			PRN.push(tmp);
-			c++;
-			break;
-		case '/':
-			tmp = PRN.pop();
-			PRN.push(PRN.pop()/tmp);
-			c++;
-			break;
-		/*case '%':
-			tmp = PRN.pop();
-			tmp %= PRN.pop();
-			PRN.push(tmp);
-			c++;
-			break;*/
+			if (*c == 0)  break;
+			switch (*c)
+			{
+			case '+':
+				tmp = PRN.pop();
+				tmp += PRN.pop();
+				PRN.push(tmp);
+				c++;
+				break;
+			case '-':
+				tmp = PRN.pop();
+				PRN.push(PRN.pop() - tmp);
+				c++;
+				break;
+			case '*':
+				tmp = PRN.pop();
+				tmp *= PRN.pop();
+				PRN.push(tmp);
+				c++;
+				break;
+			case '/':
+				tmp = PRN.pop();
+				PRN.push(PRN.pop() / tmp);
+				c++;
+				break;
+				/*case '%':
+					tmp = PRN.pop();
+					tmp %= PRN.pop();
+					PRN.push(tmp);
+					c++;
+					break;*/
+			}
+			float nmb = strtod(c, &ptrEnd);
+			if (nmb == 0 && *c != '0') { c++; continue; }
+			PRN.push(nmb);
+			c = ptrEnd;
 		}
-		float nmb = strtod(c, &ptrEnd);  //ищем в строке число
-		if (nmb == 0 && *c != '0') { c++; continue; } //если функция выдала 0, то проверяем что это, и, если ерунда какая-то, а не ноль, то в начало цикла
-		PRN.push(nmb);
-		c = ptrEnd;
+		cout << "Результат выражения равен " << PRN.pop() << endl;
+		delete[] str;
 	}
-	cout << "Результат выражения равен " << (float)PRN.pop() << " " << (4.6 + 8.9) * 2 - 7.9 / (4 - 6.5)<<endl; //исходное выражение в нормальном виде для проверки
 	
 	/*char k;
 	do{
